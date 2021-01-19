2a1274f12a7e7f7bb879632fd1c04faf1e9d2316
ethankclam <ethankclam@gmail.com>
2021-01-19 17:59:14 +0800

    Revised Login Fields

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -6,18 +6,21 @@ import { Auth } from 'aws-amplify';
 import { useAppContext } from '../libs/contextLib';
 import { useHistory } from 'react-router-dom';
 import { onError } from '../libs/errorLib';
+import { useFormFields } from '../libs/hooksLib';
 
 
 export default function Login() {
     const { userHasAuthenticated } = useAppContext();
-    const [ email, setEmail ] = useState("");
-    const [ password, setPassword ] = useState("");
     const history = useHistory();
     console.log(history);
     const [isLoading, setIsLoading] = useState(false);
+    const [fields, handleFieldChange] = useFormFields({
+        email: "",
+        password: ""
+    });
 
     async function validationForm() {
-        return email.length > 0 && password.length > 0;
+        return fields.email.length > 0 && fields.password.length > 0;
     }
 
     async function handleSubmit(event) {
@@ -26,7 +29,7 @@ export default function Login() {
         setIsLoading(true);
 
         try {
-            await Auth.signIn(email, password);
+            await Auth.signIn(fields.email, fields.password);
             userHasAuthenticated(true);
             history.push("/");
         } catch (e) {
@@ -40,11 +43,11 @@ export default function Login() {
             <Form onSubmit={handleSubmit} >
                 <Form.Group size="lg" controlId="email">
                     <Form.Label>Email</Form.Label>
-                    <Form.Control autoFocus type="email" value={email} onChange={(e)=>setEmail(e.target.value)} />
+                    <Form.Control autoFocus type="email" value={fields.email} onChange={handleFieldChange} />
                 </Form.Group>
                 <Form.Group size="lg" controlId="password"> 
                         <Form.Label>Password</Form.Label>
-                        <Form.Control type="password" value={password} onChange={(e)=>setPassword(e.target.value)} />
+                        <Form.Control type="password" value={fields.password} onChange={handleFieldChange} />
                 </Form.Group>
                 <LoaderButton block size="lg" type="submit" isLoading={isLoading} disabled={!validationForm()}>Login</LoaderButton>
             </Form>
