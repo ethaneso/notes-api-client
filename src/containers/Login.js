aecbf66ffc4b2bdf3a244a35b44ac86b4dc930fa
ethankclam <ethankclam@gmail.com>
2021-01-24 02:13:24 +0800

    Building react app

--- src/containers/Login.js
+++ src/containers/Login.js
@@ -4,15 +4,15 @@ import LoaderButton from '../components/LoaderButton';
 import './Login.css';
 import { Auth } from 'aws-amplify';
 import { useAppContext } from '../libs/contextLib';
-import { useHistory } from 'react-router-dom';
+
 import { onError } from '../libs/errorLib';
 import { useFormFields } from '../libs/hooksLib';
 
 
 export default function Login() {
     const { userHasAuthenticated } = useAppContext();
-    const history = useHistory();
-    console.log(history);
+    
+    
     const [isLoading, setIsLoading] = useState(false);
     const [fields, handleFieldChange] = useFormFields({
         email: "",
@@ -31,7 +31,7 @@ export default function Login() {
         try {
             await Auth.signIn(fields.email, fields.password);
             userHasAuthenticated(true);
-            history.push("/");
+            
         } catch (e) {
             onError(e);
             setIsLoading(false);
